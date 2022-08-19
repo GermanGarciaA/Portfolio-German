a6cc58c164727fd0c8049159d7b5b3dca7e28bb7
German Garcia <germangarcia.pf@gmail.com>
2022-08-18 22:28:15 -0300

    Desafio complementario JS

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// DESAFIO COMPLENTARIO 1
+
+let adivinanza = 'pera';
+
+for (let i = 0; i < 3; i++) {
+
+    let respuestaUsuario = prompt('Adivinanza: Blanca por dentro, verde por fuera. Si no sabes que es, espera');
+    if (adivinanza === respuestaUsuario){
+        alert('Respuesta correcta');
+        break;
+    } else{
+        alert('Te quedan ' + (2-i) + ' Intentos');
+    }
+}
