f69111e1ff4613045453e249f3170df3bc8822d5
German Garcia <germangarcia.pf@gmail.com>
2022-08-26 11:47:44 -0300

    Desafio 2

--- js/main.js
+++ js/main.js
@@ -1,16 +1,57 @@
-// DESAFIO COMPLENTARIO 1
+// SIMULADOR DE INDICE DE MASA CORPORAL
 
-let adivinanza = 'pera';
+alert('Si usted quiere saber su IMC a ingresado al lugar perfecto \nEn esta pagina calcularemos el IMC segun la formula Quetelet');
 
-for (let i = 0; i < 3; i++) {
+function ingresoDeUsuario() {
+    let nombre = prompt('Ingrese su nombre');
+    while ((!isNaN(nombre))) {
+        alert('Error: ingrese un nombre valido');
+        nombre = prompt('Ingrese su nombre');
+    }
+    
+    let apellido = prompt('Ingrese su apellido');
+    while ((!isNaN(apellido))) {
+        alert('Error: ingrese un apellido valido');
+        apellido = prompt('Ingrese su apellido');
+    }
 
-    let respuestaUsuario = prompt('Adivinanza: Blanca por dentro, verde por fuera. Si no sabes que es, espera');
-    if (adivinanza === respuestaUsuario){
-        alert('Respuesta correcta');
-        break;
-    } else{
-        alert('Te quedan ' + (2-i) + ' intentos');
-    } if (i == 2) {
-        alert('Llegaste al maximo de respuestas incorrectas')   
+    alert('Bienvenido/a ' + nombre + ' ' + apellido);
+}
+
+ingresoDeUsuario()
+
+let pesoDelUsuario = 0;
+
+function peso() {
+    pesoDelUsuario = parseFloat(prompt('Ingrese su peso en kg'));
+    while ((isNaN(pesoDelUsuario))) {
+        alert('El valor debe ser un número')
+        pesoDelUsuario = parseFloat(prompt('Ingrese su peso'));
     }
 }
+
+peso();
+
+let alturaDelUsuario = 0;
+
+function altura() {
+    alturaDelUsuario = parseFloat(prompt('Ingrese su altura en metros'));
+    while ((isNaN(alturaDelUsuario))) {
+        alert('El valor debe ser un número')
+        alturaDelUsuario = parseFloat(prompt('Ingrese su altura'));
+    }
+}
+
+altura();
+
+alert('Su peso es de ' + pesoDelUsuario + ' kg' + ' y su altura es de ' + alturaDelUsuario + ' metros');
+
+let imc = 0;
+
+function imcDelUsuario() {
+    imc = pesoDelUsuario / (alturaDelUsuario*alturaDelUsuario);
+}
+
+imcDelUsuario();
+
+alert('Su Indice de masa corporal es de: ' + imc)
